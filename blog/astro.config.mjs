b2345133598c8076e0c8860b01f30c9a6b066745
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-07 18:30:02 +0300

    static apckage

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
     adapter: vercel({
-      webAnalytics: { enabled: true }
+        webAnalytics: {
+            enabled: true,
+        },
     }),
     site: 'https://example.com',
     integrations: [
