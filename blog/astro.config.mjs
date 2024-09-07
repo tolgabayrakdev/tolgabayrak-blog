8c52b3f82d2f9526bc1fa89d0a180a86d678ecb5
tolgabayrakdev <bayraktolga28@gmail.com>
2024-09-07 18:34:04 +0300

    rconfig

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
     site: 'https://example.com',
     integrations: [
         mdx(),
